1c39785cd370970edda033a0dbd2ad30a10aeaaa
aslan <113269243+retkets17@users.noreply.github.com>
2025-11-05 16:55:10 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T13:54:46.040Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2025-11-05T13:55:10.299Z
+Commit: Patch API endpoint error handling

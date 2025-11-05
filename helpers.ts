54258e38cceb53de3988c4bfc42cce537e5605a9
aslan <113269243+retkets17@users.noreply.github.com>
2025-11-05 16:51:15 +0300

    Fix database connection timeout

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T13:51:05.027Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2025-11-05T13:51:13.549Z
+Commit: Fix database connection timeout

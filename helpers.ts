6d28caaf1e2f3ec3b09797d6d91e6a9df78603c3
aslan <113269243+retkets17@users.noreply.github.com>
2025-11-05 22:19:12 +0300

    Introduce real-time notifications

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T19:18:10.061Z
-Commit: Correct typo in configuration file
+Timestamp: 2025-11-05T19:19:12.662Z
+Commit: Introduce real-time notifications

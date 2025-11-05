b5b7e0d141c7cfe8a8a3be8dd0f561d345754090
aslan <113269243+retkets17@users.noreply.github.com>
2025-11-05 22:29:07 +0300

    Patch API endpoint error handling

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T19:20:10.059Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2025-11-05T19:29:05.206Z
+Commit: Patch API endpoint error handling

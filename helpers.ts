1e609afe36f0aab70b730d9ee89f644977575c4d
aslan <113269243+retkets17@users.noreply.github.com>
2025-11-05 22:30:33 +0300

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T19:30:20.812Z
-Commit: Implement user profile customization
+Timestamp: 2025-11-05T19:30:31.427Z
+Commit: Fix broken links in documentation

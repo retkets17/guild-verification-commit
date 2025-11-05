d399ea04b0422d37b6c00ddc21d542807c36308b
aslan <113269243+retkets17@users.noreply.github.com>
2025-11-05 22:30:04 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T19:29:48.841Z
-Commit: Fix broken links in documentation
+Timestamp: 2025-11-05T19:30:02.777Z
+Commit: Resolve null pointer exception in user service

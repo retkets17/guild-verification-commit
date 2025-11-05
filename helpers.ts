ba68219cfffd18a1d3453c0936428f4c712c68f5
aslan <113269243+retkets17@users.noreply.github.com>
2025-11-05 22:30:39 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T19:30:31.427Z
-Commit: Fix broken links in documentation
+Timestamp: 2025-11-05T19:30:37.488Z
+Commit: Patch API endpoint error handling

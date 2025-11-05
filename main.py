ca331db700027c4ec51e4e4f2ed2f33a8f3589e6
aslan <113269243+retkets17@users.noreply.github.com>
2025-11-05 22:29:52 +0300

    Resolve null pointer exception in user service

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T19:29:19.560Z
-Commit: Introduce real-time notifications
+Timestamp: 2025-11-05T19:29:50.227Z
+Commit: Resolve null pointer exception in user service

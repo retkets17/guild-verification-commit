50c8f3a548b2d9301a085cfdc8fd22bf286e6ed6
aslan <113269243+retkets17@users.noreply.github.com>
2025-11-05 22:17:48 +0300

    Fix CSS styling bug on mobile devices

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T19:17:38.001Z
-Commit: Fix broken links in documentation
+Timestamp: 2025-11-05T19:17:49.224Z
+Commit: Fix CSS styling bug on mobile devices

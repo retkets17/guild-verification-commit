3f5293f01c74c3998dde8258eed9e34454c9da53
aslan <113269243+retkets17@users.noreply.github.com>
2025-11-05 22:17:50 +0300

    Correct typo in configuration file

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T19:17:49.224Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2025-11-05T19:17:50.812Z
+Commit: Correct typo in configuration file

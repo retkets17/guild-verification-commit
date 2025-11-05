5300847315641d2c06eb74ddb2005cc0343e32c7
aslan <113269243+retkets17@users.noreply.github.com>
2025-11-05 22:29:50 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T19:29:21.114Z
-Commit: Patch API endpoint error handling
+Timestamp: 2025-11-05T19:29:48.841Z
+Commit: Fix broken links in documentation

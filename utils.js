2f10e1d5f974b792274d6e77bede3cddc5565412
aslan <113269243+retkets17@users.noreply.github.com>
2025-11-05 22:19:40 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T19:19:38.699Z
-Commit: Add export functionality for reports
+Timestamp: 2025-11-05T19:19:41.006Z
+Commit: Fix broken links in documentation

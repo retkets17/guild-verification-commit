db363a23a52321d0a77e7ce0258c142312302b2f
aslan <113269243+retkets17@users.noreply.github.com>
2025-11-05 16:55:45 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T13:55:36.767Z
-Commit: Correct typo in configuration file
+Timestamp: 2025-11-05T13:55:45.347Z
+Commit: Fix memory leak in event handlers

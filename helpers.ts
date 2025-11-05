24cdbcc7efd091ce83ff053a41a9ca47d7cefd5c
aslan <113269243+retkets17@users.noreply.github.com>
2025-11-05 16:55:13 +0300

    Introduce real-time notifications

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T13:55:10.299Z
-Commit: Patch API endpoint error handling
+Timestamp: 2025-11-05T13:55:13.099Z
+Commit: Introduce real-time notifications

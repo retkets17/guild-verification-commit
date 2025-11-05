41af50071f4e95ba3cd92fca4a86f00134021660
aslan <113269243+retkets17@users.noreply.github.com>
2025-11-05 22:29:22 +0300

    Patch API endpoint error handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T19:28:51.006Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2025-11-05T19:29:21.114Z
+Commit: Patch API endpoint error handling

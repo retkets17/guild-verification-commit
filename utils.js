cf60dc89ed9ee45e692e0d368d8a9158a68f0f96
aslan <113269243+retkets17@users.noreply.github.com>
2025-11-05 22:19:29 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T19:19:14.441Z
-Commit: Fix broken links in documentation
+Timestamp: 2025-11-05T19:19:30.507Z
+Commit: Fix memory leak in event handlers

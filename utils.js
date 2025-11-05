cf31e270a24ba465ddff466917c73513392742a9
aslan <113269243+retkets17@users.noreply.github.com>
2025-11-05 22:30:50 +0300

    Add export functionality for reports

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T19:30:02.777Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2025-11-05T19:30:48.301Z
+Commit: Add export functionality for reports

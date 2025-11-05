af8cad60bf12ced59ae7e6dc89799c795d6f0dc8
aslan <113269243+retkets17@users.noreply.github.com>
2025-11-05 22:19:03 +0300

    Fix memory leak in event handlers

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T19:18:19.684Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2025-11-05T19:19:04.264Z
+Commit: Fix memory leak in event handlers
